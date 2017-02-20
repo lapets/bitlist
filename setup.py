9731a0db87b9db71ab16930b2383973caf027a0c
Andrei Lapets <a@lapets.io>
2017-02-19 20:02:01 -0500

    Update module version number in package file.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name             = 'bitlist',
-    version          = '0.0.3.0',
+    version          = '0.0.4.0',
     packages         = ['bitlist',],
     install_requires = [],
     license          = 'MIT License',
