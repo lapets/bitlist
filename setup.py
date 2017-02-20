7f62df2706c06906f90f74a920bb9e3c9751e0a4
Andrei Lapets <a@lapets.io>
2017-02-19 20:13:05 -0500

    Update to latest package version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name             = 'bitlist',
-    version          = '0.0.4.0',
+    version          = '0.0.8.0',
     packages         = ['bitlist',],
     install_requires = [],
     license          = 'MIT License',
